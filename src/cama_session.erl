db062cf9687ac8c11c7288d389d3197bb0292a4d
Sungjin Park <jinni.park@gmail.com>
2012-05-10 16:45:54 +0900

    Added receiver logic.

--- src/cama_session.erl
+++ src/cama_session.erl
@@ -23,12 +23,13 @@
 
 %% gen_fsm callbacks
 -export([init/1, handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
--export(['IDLE'/2, 'IDLE'/3]).
+-export(['IDLE'/2, 'IDLE'/3, 'BUSY'/2, 'BUSY'/3]).
 
 -record(?MODULE, {id,
 				  token,
 				  created,
 				  timeout = 3600,
+				  request_timeout = 300,
 				  type = basic,
 				  pid,
 				  timer,
@@ -80,6 +81,7 @@ out(Arg) ->
 %%          {stop, StopReason}
 %% --------------------------------------------------------------------
 init(Session) ->
+	process_flag(trap_exit, true),
 	Session1 = Session#?MODULE{pid = self()},
 	{atomic, ok} = mnesia:transaction(fun() -> mnesia:write(Session1) end),
 	Session2 = Session1#?MODULE{timer = gen_fsm:start_timer(Session1#?MODULE.timeout*1000, expired)},
@@ -99,6 +101,16 @@ init(Session) ->
     {next_state, 'IDLE', StateData}.
 
 %% --------------------------------------------------------------------
+%% Func: 'BUSY'/2
+%% Returns: {next_state, NextStateName, NextStateData}          |
+%%          {next_state, NextStateName, NextStateData, Timeout} |
+%%          {stop, Reason, NewStateData}
+%% --------------------------------------------------------------------
+'BUSY'(Event, StateData) ->
+	?WARNING(["undefined async event", Event]),
+    {next_state, 'BUSY', StateData}.
+
+%% --------------------------------------------------------------------
 %% Func: 'IDLE'/3
 %% Returns: {next_state, NextStateName, NextStateData}            |
 %%          {next_state, NextStateName, NextStateData, Timeout}   |
@@ -107,10 +119,66 @@ init(Session) ->
 %%          {stop, Reason, NewStateData}                          |
 %%          {stop, Reason, Reply, NewStateData}
 %% --------------------------------------------------------------------
+'IDLE'(host_start_receiving, {Receiver, _}, Session) ->
+	?DEBUG(["host", Session#?MODULE.id, "started receiving from", Receiver]),
+	link(Receiver),
+	gen_fsm:cancel_timer(Session#?MODULE.timer),
+	{reply, ok, 'BUSY', Session#?MODULE{hostr=Receiver, timer=undefined}};
+'IDLE'(guest_start_receiving, {Receiver, _}, Session) ->
+	?DEBUG(["guest", Session#?MODULE.id, "started receiving from", Receiver]),
+	link(Receiver),
+	gen_fsm:cancel_timer(Session#?MODULE.timer),
+	{reply, ok, 'BUSY', Session#?MODULE{guestr=Receiver, timer=undefined}};
 'IDLE'(Event, From, StateData) ->
 	?WARNING(["undefined sync event", Event, From]),
     Reply = ok,
     {reply, Reply, 'IDLE', StateData}.
+
+%% --------------------------------------------------------------------
+%% Func: 'BUSY'/3
+%% Returns: {next_state, NextStateName, NextStateData}            |
+%%          {next_state, NextStateName, NextStateData, Timeout}   |
+%%          {reply, Reply, NextStateName, NextStateData}          |
+%%          {reply, Reply, NextStateName, NextStateData, Timeout} |
+%%          {stop, Reason, NewStateData}                          |
+%%          {stop, Reason, Reply, NewStateData}
+%% --------------------------------------------------------------------
+'BUSY'(host_start_receiving, {Receiver, _}, Session=#?MODULE{hostr=Hostr}) ->
+	?DEBUG(["host", Session#?MODULE.id, "started receiving from", Receiver]),
+	case (Hostr =:= undefined) or (Hostr =:= Receiver) of
+		true ->
+			link(Receiver),
+			case Session#?MODULE.guests of
+				undefined ->
+					ok;
+				Sender ->
+					Sender ! {receiver_arrived, Receiver}
+			end,
+			{reply, ok, 'BUSY', Session#?MODULE{hostr=Receiver}};
+		_ ->
+			?WARNING(["another host", Hostr, "is already receiving"]),
+			{reply, occupied, 'BUSY', Session}
+	end;
+'BUSY'(guest_start_receiving, {Receiver, _}, Session=#?MODULE{guestr=Guestr}) ->
+	?DEBUG(["guest", Session#?MODULE.id, "started receiving from", Receiver]),
+	case (Guestr =:= undefined) or (Guestr =:= Receiver) of
+		true ->
+			link(Receiver),
+			case Session#?MODULE.hosts of
+				undefined ->
+					ok;
+				Sender ->
+					Sender ! {receiver_arrived, Receiver}
+			end,
+			{reply, ok, 'BUSY', Session#?MODULE{guestr=Receiver}};
+		_ ->
+			?WARNING(["another guest", Guestr, "is already receiving"]),
+			{reply, occupied, 'BUSY', Session}
+	end;
+'BUSY'(Event, From, StateData) ->
+	?WARNING(["undefined sync event", Event, From]),
+    Reply = ok,
+    {reply, Reply, 'BUSY', StateData}.
 
 %% --------------------------------------------------------------------
 %% Func: handle_event/3
@@ -142,6 +210,44 @@ handle_sync_event(Event, _From, StateName, StateData) ->
 %%          {next_state, NextStateName, NextStateData, Timeout} |
 %%          {stop, Reason, NewStateData}
 %% --------------------------------------------------------------------
+handle_info({'EXIT', Pid, _Reason}, 'BUSY', Session=#?MODULE{hostr=Hostr, hosts=Hosts, guestr=Guestr, guests=Guests}) ->
+	case Pid of
+		Hostr ->
+			?DEBUG(["host", Session#?MODULE.id, "stopped receiving from", Pid]),
+			case (Hosts =:= undefined) and (Guestr =:= undefined) and (Guests =:= undefined) of
+				true ->
+					{next_state, 'IDLE', Session#?MODULE{hostr=undefined, timer=gen_fsm:start_timer(Session#?MODULE.timeout*1000, expired)}};
+				_ ->
+					{next_state, 'BUSY', Session#?MODULE{hostr=undefined}}
+			end;
+		Hosts ->
+			?DEBUG(["host", Session#?MODULE.id, "stopped sending from", Pid]),
+			case (Hostr =:= undefined) and (Guestr =:= undefined) and (Guests =:= undefined) of
+				true ->
+					{next_state, 'IDLE', Session#?MODULE{hosts=undefined, timer=gen_fsm:start_timer(Session#?MODULE.timeout*1000, expired)}};
+				_ ->
+					{next_state, 'BUSY', Session#?MODULE{hosts=undefined}}
+			end;
+		Guestr ->
+			?DEBUG(["guest", Session#?MODULE.id, "stopped receiving from", Pid]),
+			case (Hostr =:= undefined) and (Hosts =:= undefined) and (Guests =:= undefined) of
+				true ->
+					{next_state, 'IDLE', Session#?MODULE{guestr=undefined, timer=gen_fsm:start_timer(Session#?MODULE.timeout*1000, expired)}};
+				_ ->
+					{next_state, 'BUSY', Session#?MODULE{guestr=undefined}}
+			end;
+		Guests ->
+			?DEBUG(["guest", Session#?MODULE.id, "stopped sending from", Pid]),
+			case (Hosts =:= undefined) and (Hostr =:= undefined) and (Guestr =:= undefined) of
+				true ->
+					{next_state, 'IDLE', Session#?MODULE{guests=undefined, timer=gen_fsm:start_timer(Session#?MODULE.timeout*1000, expired)}};
+				_ ->
+					{next_state, 'BUSY', Session#?MODULE{guests=undefined}}
+			end;
+		_ ->
+			?WARNING(["session", Session#?MODULE.id, "received unknown exit from", Pid]),
+			{next_state, 'BUSY', Session}
+	end;
 handle_info(_Info, StateName, StateData) ->
     {next_state, StateName, StateData}.
 
@@ -225,17 +331,73 @@ recv(Arg, Sid) ->
 			 cama_dispatcher:server_header(Arg)]
 	end.
 
-%% @todo impl
+%% Host registers itself to session and start receiving.
 host_recv(Arg, Session) ->
-	[{status, 200},
-	 cama_dispatcher:server_header(Arg),
-	 {content, "text/plain", "You are host!"}].
+	case gen_fsm:sync_send_event(Session#?MODULE.pid, host_start_receiving) of
+		ok ->
+			Timeout = case yaws_api:queryvar(Arg, "timeout") of
+						  {ok, Val} ->
+							  case string:to_integer(Val) of
+								  {error, _} ->
+									  Session#?MODULE.request_timeout;
+								  {Int, _} ->
+									  Int
+							  end;
+						  _ ->
+							  Session#?MODULE.request_timeout
+					  end,
+			recv_loop(Arg, Timeout);
+		occupied ->
+			[{status, 409},
+			 cama_dispatcher:server_header(Arg),
+			 {content, "text/plain", "there can be only one receiver at a time"}];
+		_ ->
+			[{status, 500},
+			 cama_dispatcher:server_header(Arg),
+			 {content, "text/plain", "session doesn't resond"}]
+	end.
 
-%% @todo impl
+%% Guest registers itself to session and start receiving.
 guest_recv(Arg, Session) ->
-	[{status, 200},
-	 cama_dispatcher:server_header(Arg),
-	 {content, "text/plain", "You are guest!"}].
+	case gen_fsm:sync_send_event(Session#?MODULE.pid, guest_start_receiving) of
+		ok ->
+			Timeout = case yaws_api:queryvar(Arg, "timeout") of
+						  {ok, Val} ->
+							  case string:to_integer(Val) of
+								  {error, _} ->
+									  Session#?MODULE.request_timeout;
+								  {Int, _} ->
+									  Int
+							  end;
+						  _ ->
+							  Session#?MODULE.request_timeout
+					  end,
+			recv_loop(Arg, Timeout);
+		occupied ->
+			[{status, 409},
+			 cama_dispatcher:server_header(Arg),
+			 {content, "text/plain", "there can be only one receiver at a time"}];
+		_ ->
+			[{status, 500},
+			 cama_dispatcher:server_header(Arg),
+			 {content, "text/plain", "session doesn't resond"}]
+	end.
+
+recv_loop(Arg, Timeout) ->
+	receive
+		{chunk, ContentType, Data} ->
+			[{status, 200},
+			 cama_dispatcher:server_header(Arg),
+			 {streamcontent, ContentType, Data}];
+		{data, ContentType, Data} ->
+			[{status, 200},
+			 cama_dispatcher:server_header(Arg),
+			 {content, ContentType, Data}]
+	after Timeout*1000 ->
+			[{status, 504},
+			 cama_dispatcher:server_header(Arg),
+			 {content, "text/plain", "request timed out"}]
+	end.
 
 age2expire(Age) ->
 	Now = calendar:datetime_to_gregorian_seconds(calendar:local_time()),
